9fb7b590e319155f171e164e471e1d067decd47b
doralado <167039188+doralado@users.noreply.github.com>
2024-05-17 11:07:38 +0300

    Update Main.java - main page ekledim

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,34 @@ import java.util.Calendar;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static void mainpage(){
+        String username=Login_Register.username;
+        System.out.print("Welcome "+username+", What would you like to do?"+"\nShow Info\nMy Diet\nCalorie Library\nSports\n");
+        Scanner sc=new Scanner (System.in);
+        String choice_main_page=sc.nextLine();
+        choice_main_page=choice_main_page.replaceAll("\\s", "");
+
+        switch(choice_main_page.toLowerCase()){
+            case "showinfo":
+                String gender=Login_Register.gender;
+                Float weight=Login_Register.weight;
+                Float height=Login_Register.height;
+                Float weight_goal=Login_Register.weight_goal;
+
+                System.out.println("Gender: "+gender+"\n"+"Weight: "+weight+"\n"+"Height: "+height+"\n"+"Weight Goal: "+weight_goal+"\n"+"Body Mass Index: "+(weight/((height/100)*(height/100))));
+                break;
+                case "mydiet":
+
+                    break;
+                    case "calorielibrary":
+
+                        break;
+                        case "sports":
+
+                            break;
+        }
+
+    }
     public static void main(String[] args) {
         Calendar calendar = Calendar.getInstance();
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
@@ -35,8 +63,7 @@ public class Main {
                         } catch (SQLException e) {
                             throw new RuntimeException(e);
                         }
-                        String gender=Login_Register.gender;
-                        System.out.println(gender);
+                        mainpage();
                         break;
                     case "register" : break;
                     case "exit" : break;
