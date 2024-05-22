0e873aff80179d2929b7a3b9d3d9b8c659785ba2
Decadence <enesdgnr666@gmail.com>
2024-05-22 21:13:08 +0300

    Login_Register ve Main de degisiklikler yapildi -Register kisaltmasi yapildi -Yeni Class User_Info acildi -Pregnancy boolean a cevrildi -Usernameler unique yapildi -Main yeni classlara göre guncellendi

--- src/Login_Register.java
+++ src/Login_Register.java
@@ -1,102 +1,101 @@
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.InputMismatchException;
-import java.util.Scanner;
 
 public class Login_Register {
-    public static String gender;
-    public static Float weight;
-    public static Float height;
-    public static Float weight_goal;
-    public static String username;
-    public static String password;
-    public static String pregnancy;
-    public static boolean login_check = false;//login check
-    public static void login(Scanner sc) throws SQLException {
-//        Establish  connection to DB
+    public static void login() throws SQLException {
+
+//        Establish and test connection to DB
         ConnectionDB db = new ConnectionDB();
         db.connect();
 //        Get the info
         System.out.println("Enter Username");
-        username = sc.next();
+        User_Info.username = Main.sc.next();
         System.out.println("Enter Password");
-        password = sc.next();
+        User_Info.password = Main.sc.next();
 
         PreparedStatement user_check;
         user_check = db.connect().prepareStatement("select * from users");
         ResultSet rs = user_check.executeQuery();
 
         while (rs.next()) {
-            if (rs.getString("username").equals(username) && rs.getString("password").equals(password)) {
-                System.out.println("Login Successful");
-                login_check = true; //login check
-                gender =rs.getString("gender");
-                 weight =rs.getFloat("weight");
-                 height =rs.getFloat("heigth");
-                 weight_goal =rs.getFloat("goal_w");
+            if (rs.getString("username").equals(User_Info.username) && rs.getString("password").equals(User_Info.password)) {
+
+                User_Info.login_check = true;
+                User_Info.gender = rs.getString("gender");
+                User_Info.weight = rs.getFloat("weight");
+                User_Info.height = rs.getFloat("heigth");
+                User_Info.weight_goal = rs.getFloat("goal_w");
+                User_Info.pregnancy = rs.getBoolean("pregnancy");
+                User_Info.bmi = rs.getFloat("bmi");
+                break;
             } else {
-                System.out.println("Login Failed\n" +
-                        "Incorrect Username or Password");
-                login_check = false; //login check
+                User_Info.login_check = false;
             }
+        }
+        if(User_Info.login_check == true) {
+            System.out.println("Login Successful");
+        } else {
+            System.out.println("Login Failed");
         }
     }
 
-    public static void register(Scanner sc) {
+    public static void register() throws SQLException {
         ConnectionDB db = new ConnectionDB();
         db.connect();
 
-        //get id from user
-        System.out.print("Enter username:\n");
-        username = sc.nextLine();
-        //get pw from user
-        System.out.print("Enter password:\n");
-        password = sc.nextLine();
-        System.out.print("Enter gender: (Male/Female)\n");
-        do {
-            gender = sc.nextLine();
-            if (!(gender.equalsIgnoreCase("male")) && !(gender.equals("female"))) {
-                System.out.println("Please enter a valid gender");
-            }
-        } while (!(gender.equalsIgnoreCase("male")) && !(gender.equals("female")));
+        try{PreparedStatement register = db.connect().prepareStatement("INSERT INTO users" +
+                "(username, password, gender, heigth, weight, goal_w, bmi, pregnancy)values" +
+                "(?, ?, ?, ?, ?, ?, ?, ?)");
+            {
+                System.out.print("Enter username:\n");
+                PreparedStatement if_user_exist;
+                if_user_exist = db.connect().prepareStatement("select username from users where username=?");
+                while(true) {
+                    register.setString(1, User_Info.username=Main.sc.next());
+                    if_user_exist.setString(1, User_Info.username);
+                    ResultSet rs1 = if_user_exist.executeQuery();
+                    if (rs1.next()) {
+                        System.out.print("Username already exists. Please enter another name:\n");
+                    }
+                    else{
+                        break;
+                    }
+                }
+                System.out.print("Enter password:\n");
+                register.setString(2, User_Info.password=Main.sc.next());
+                System.out.print("Enter gender:\n");
+                do {
+                    register.setString(3, User_Info.gender=Main.sc.next());
+                    if (!(User_Info.gender.equalsIgnoreCase("male")) && !(User_Info.gender.equals("female"))) {
+                        System.out.println("Please enter a valid gender\n");
+                    }
+                } while (!(User_Info.gender.equalsIgnoreCase("male")) && !(User_Info.gender.equals("female")));
 
-        if(gender.equalsIgnoreCase("female")) {
-            System.out.print("Are you pregnant? Yes/No");
-            pregnancy = sc.next();
-            if(pregnancy.equalsIgnoreCase("yes")) {
-                System.out.println("Congratulations! :)\nKeep in mind that your BMI may not be accurate and instead of focusing on your weight your priority is a healthy diet.");
-            }
-        }
-        
-        System.out.print("Enter weight(kg):");
-        weight = sc.nextFloat();
-        System.out.print("Enter height(cm):\n");
-         height = sc.nextFloat();
-        System.out.print("Enter your target weight(kg):\n");
-        weight_goal = sc.nextFloat();
-
-        try{
-            PreparedStatement register = db.connect().prepareStatement("INSERT INTO users" +
-                    "(username, password, gender, heigth, weight, goal_w, bmi, pregnancy)values" +
-                            "(?, ?, ?, ?, ?, ?, ?, ?)");{
-                register.setString(1, username);
-                register.setString(2, password);
-                register.setString(3, gender);
-                register.setFloat(4, height);
-                register.setFloat(5, weight);
-                register.setFloat(6, weight_goal);
-                register.setFloat(7, (float) (weight/(Math.sqrt(height/100))));
-                register.setString(8, pregnancy);
+                if(User_Info.gender.equalsIgnoreCase("female")) {
+                    System.out.print("Are you pregnant? Yes/No\n");
+                    if(Main.sc.next().equalsIgnoreCase("yes")) {
+                        System.out.println("Congratulations! :)\nKeep in mind that your BMI may not be accurate and instead of focusing on your weight your priority is a healthy diet.");
+                        register.setBoolean(8, true);
+                    }
+                    else {
+                        register.setBoolean(8, false);
+                    }
+                }
+                System.out.print("Enter height(cm):\n");
+                register.setFloat(4, User_Info.height=Main.sc.nextFloat());
+                System.out.print("Enter weight(kg):");
+                register.setFloat(5, User_Info.weight=Main.sc.nextFloat());
+                System.out.print("Enter your target weight(kg):\n");
+                register.setFloat(6, User_Info.weight_goal=Main.sc.nextFloat());
+                User_Info.bmi = (User_Info.weight / (((User_Info.height / 100))*(User_Info.height / 100)));
+                register.setFloat(7,User_Info.bmi);
                 System.out.println(register);
                 register.executeUpdate();
-
             }
-
         }
         catch (SQLException e) {
             System.out.println("Error inserting user ");
         }
-
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,80 +1,35 @@
 import java.sql.SQLException;
 import java.util.Scanner;
-import java.util.Calendar;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void mainpage(){
-        String username=Login_Register.username;
-        System.out.print("Welcome "+username+", What would you like to do?"+"\nShow Info\nMy Diet\nCalorie Library\nSports\n");
-        Scanner sc=new Scanner (System.in);
-        String choice_main_page=sc.nextLine();
-        choice_main_page=choice_main_page.replaceAll("\\s", "");
-
-        switch(choice_main_page.toLowerCase()){
-            case "showinfo":
-                 System.out.println("Gender: "+Login_Register.gender+"\n"+"Weight: "+Login_Register.weight+"\n"+"Height: "+Login_Register.height+"\n"+"Weight Goal: "+Login_Register.weight_goal+"\n"+"Body Mass Index: "+(Login_Register.weight/((Login_Register.height/100)*(Login_Register.height/100))));
-                break;
-                case "mydiet":
-
-                    break;
-                    case "calorielibrary":
-
-                        break;
-                        case "sports":
-
-                            break;
-        }
-
-    }
-    public static void main(String[] args) {
-        Calendar calendar = Calendar.getInstance();
-        int hour = calendar.get(Calendar.HOUR_OF_DAY);
-    
-        if (hour<12 && hour>=5){
-            System.out.print("Goodmorning.Happy to see you here :) .\n");
-         }
-        else if(hour<20 && hour>=12){
-            System.out.print("Goodevening.Happy to see you here :) .\n");
-        }
-        else{
-            System.out.print("Goodnight.Happy to see you here :) .\nDon't stay up late!\n");
-        }
-        Scanner sc = new Scanner(System.in);
-
+    public static final Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws SQLException {
+        Home home = new Home();
         ConnectionDB db = new ConnectionDB();
         db.connect();
-
-                while(true) {
-                    System.out.printf("========Welcome========\n" +
-                        "Login\t\t=======> Login\n" +
-                        "Register\t=======> Register\n" +
-                        "Exit\t\t=======> Exit\n ");
-                    String choice = sc.next();
-                    switch(choice.toLowerCase(Locale.ROOT)){
-                        case "login":
-    
-                            try {
-                                Login_Register.login(sc);
-                            } catch (SQLException e) {
-                                throw new RuntimeException(e);
-                            }
-                            if(Login_Register.login_check==true) {
-                                mainpage();
-                            }
-                            break;
-                        case "register" : 
-                            Login_Register.register(sc);
-                            System.out.println("Thanks for registering!");
-                            break;
-                        case "exit" : break;
+        while(true) {
+            System.out.printf("========Welcome========\n" +
+                    "Login\t\t=======> Login\n" +
+                    "Register\t=======> Register\n" +
+                    "Exit\t\t=======> Exit\n");
+            String choice = sc.next();
+            switch(choice.toLowerCase()){
+                case "login":
+                    Login_Register.login();
+                    if(User_Info.login_check==true) {
+                        Home.home();
                     }
-                    if(choice.toLowerCase().equals("exit")){
-                        break;
-                    }
-                }
-
+                    break;
+                case "register" :
+                    Login_Register.register();
+                    System.out.println("Thanks for registering!");
+                    break;
+                case "exit" : break;
+            }
+            if(choice.toLowerCase().equals("exit")){
+                break;
+            }
+        }
         sc.close();
     }
 }
