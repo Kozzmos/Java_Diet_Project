63082522b8fb239c95f3ea5bc1b62b9267b79399
Decadence <enesdgnr666@gmail.com>
2024-05-30 23:20:06 +0300

    -Updated sports

--- src/Sports.java
+++ src/Sports.java
@@ -13,27 +13,34 @@ public class Sports {
                 String[] sports = {"Running", "Walking", "Swimming", "Cycling", "Yoga", "Weightlifting"};
 //                double[] caloriesBurnedPerHour = {700, 500, 600, 300, 400}; bu ortalama 70 kilo biri için özel hesaplama yazicam buraya sonra
 
-                System.out.println("Select an activity to see estimated calories burned per hour:");
-                String sport_choice = sc.nextLine().toLowerCase();
+                System.out.println("Select an activity to see estimated calories burned per hour: ");
+
+                for (int i = 0; i < sports.length; i++) {
+                    System.out.println((i + 1) + ". " + sports[i]);
+                }
+
+                String sport_choice = sc.next().toLowerCase();
                 System.out.println("How many minutes you did that");
                 Float duration = sc.nextFloat()/60;
 
+                User_Info.caloriesBurned = (float) 0;
+
                 switch (sport_choice){
                     case "running":
-                        Float caloriesBurned_run = (10 * User_Info.weight * duration);
-                        System.out.println("You burned approximately" + Math.round(caloriesBurned_run) + "calories by doing" + sport_choice + "for" + duration + "hours");
+                        User_Info.caloriesBurned += (10 * User_Info.weight * duration);
+                        System.out.println("You burned approximately" + Math.round(User_Info.caloriesBurned) + "calories by doing" + sport_choice + "for" + duration + "hours");
                         break;
                     case "walking":
-                        Float caloriesBurned_walk = (3 * User_Info.weight * duration);
-                        System.out.println("You burned approximately" + Math.round(caloriesBurned_walk) + "calories by doing" + sport_choice + "for" + duration + "hours");
+                        User_Info.caloriesBurned += (3 * User_Info.weight * duration);
+                        System.out.println("You burned approximately" + Math.round(User_Info.caloriesBurned) + "calories by doing" + sport_choice + "for" + duration + "hours");
                         break;
                     case "swimming":
-                        Float caloriesBurned_swim = (5 * User_Info.weight * duration);
-                        System.out.println("You burned approximately" + Math.round(caloriesBurned_swim) + "calories by doing" + sport_choice + "for" + duration + "hours");
+                        User_Info.caloriesBurned += (5 * User_Info.weight * duration);
+                        System.out.println("You burned approximately" + Math.round(User_Info.caloriesBurned) + "calories by doing" + sport_choice + "for" + duration + "hours");
                         break;
                     case "cycling":
-                        Float caloriesBurned_cyc = (4 * User_Info.weight * duration);
-                        System.out.println("You burned approximately" + Math.round(caloriesBurned_cyc) + "calories by doing" + sport_choice + "for" + duration + "hours");
+                        User_Info.caloriesBurned += (4 * User_Info.weight * duration);
+                        System.out.println("You burned approximately" + Math.round(User_Info.caloriesBurned) + "calories by doing" + sport_choice + "for" + duration + "hours");
                         break;
                 }
 
